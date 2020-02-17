2720e057f1d94741ef5a1e96bac28eedb1fe3361
MyOde <toms.ozols5@gmail.com>
2020-02-17 14:54:17 +0100

    Externals naming

--- scripts/externals.js
+++ scripts/externals.js
@@ -8,7 +8,6 @@ const path = require('path');
  */
 const toGlobalName = name =>
   `${name}`
-    .split('/')[1]
     .replace(new RegExp(/[-_/]+/, 'g'), ' ')
     .replace(new RegExp(/[^\w\s]/, 'g'), '')
     .replace(
@@ -18,20 +17,20 @@ const toGlobalName = name =>
     .replace(new RegExp(/\s/, 'g'), '')
     .replace(new RegExp(/\w/), s => s.toUpperCase());
 
+// const toNonScopedName = name => name.split('/')[1];
+
 const packages = fs.readdirSync(path.resolve(__dirname, '../packages'));
 
 const packageExports = {};
-packages
-  .map(name => `@forgettingpasswords/${name}`)
-  .map(name => {
-    packageExports[name] = {
-      root: `${toGlobalName(name)}`.split('.'),
-      commonjs2: name,
-      commonjs: name,
-      amd: name,
-      umd: name,
-    };
-  });
+packages.map(name => {
+  packageExports[`@forgettingpasswords/${name}`] = {
+    root: `${toGlobalName(name)}`.split('.'),
+    commonjs2: name,
+    commonjs: name,
+    amd: name,
+    umd: name,
+  };
+});
 
 console.log('+++++++++++++++++++++++++++++++++++++++++++++++');
 console.log('+++++++++++++++++++++++++++++++++++++++++++++++');
