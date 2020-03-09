f3b43199218b88b89c31b80bf997e59f464271f3
MyOde <toms.ozols5@gmail.com>
2020-03-09 11:39:26 +0100

    2189 No more master ref being pulled around

--- packages/netlify-cms-core/src/actions/entryHistory.js
+++ packages/netlify-cms-core/src/actions/entryHistory.js
@@ -4,11 +4,13 @@ export const ADD_ENTRY_HISTORY = 'ADD_ENTRY_HISTORY';
 export const SELECT_ENTRY_HISTORY_ITEM = 'SELECT_ENTRY_HISTORY_ITEM';
 export const DRAFT_CREATE_FROM_ENTRY_HISTORY = 'DRAFT_CREATE_FROM_ENTRY_HISTORY';
 
-export const addEntryHistory = (key, commits) => ({
+export const addEntryHistory = (key, commits, collection, slug) => ({
   type: ADD_ENTRY_HISTORY,
   payload: {
     key,
-    commits
+    commits,
+    collection: collection.get('name'),
+    slug
   }
 });
 
@@ -17,10 +19,5 @@ export const loadEntryHistory = (collection, slug) => async (dispatch, getState)
   const backend = currentBackend(state.config);
   const commits = await backend.getFileVersions(collection, slug);
   const key = `${collection.get('name')}.${slug}`;
-  dispatch(addEntryHistory(key, commits));
+  dispatch(addEntryHistory(key, commits, collection, slug));
 };
-
-export const selectEntryHistoryItem = ref => ({
-  type: SELECT_ENTRY_HISTORY_ITEM,
-  payload: ref
-});
