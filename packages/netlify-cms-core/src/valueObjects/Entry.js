13c5c33f5add0cec6a782141e1cab889cfbc7de0
MyOde <toms.ozols5@gmail.com>
2020-06-22 14:52:11 +0200

    DPL Umlaut rewrite in the filename slug

--- packages/netlify-cms-core/src/valueObjects/Entry.js
+++ packages/netlify-cms-core/src/valueObjects/Entry.js
@@ -1,9 +1,26 @@
 import { isBoolean } from 'lodash';
 
+const umlautReplacements = {
+  Ü: 'UE',
+  Ä: 'AE',
+  Ö: 'OE',
+  ü: 'ue',
+  ä: 'ae',
+  ö: 'oe',
+  ß: 'ss'
+};
+
+const umlauts = Object.keys(umlautReplacements);
+const umlautUnion = umlauts.slice(1).reduce((acc, umlaut) => `${acc}|${umlaut}`, umlauts[0]);
+const replaceUmlautsRegex = new RegExp(umlautUnion, 'g');
+
+export const toUrlSafeUmlauts = value =>
+  value.replace(replaceUmlautsRegex, umlautMatch => umlautReplacements[umlautMatch]);
+
 export function createEntry(collection, slug = '', path = '', options = {}) {
   const returnObj = {};
   returnObj.collection = collection;
-  returnObj.slug = slug;
+  returnObj.slug = toUrlSafeUmlauts(slug);
   returnObj.path = path;
   returnObj.partial = options.partial || false;
   returnObj.raw = options.raw || '';
